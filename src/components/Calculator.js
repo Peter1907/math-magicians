dc168c201e39005c3b343eaaba592f6393dbb984
Peter1907 <peter.nady.1408@gmail.com>
2022-09-16 00:13:47 +0200

    Refractor the calculator class into func & Use hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,57 +1,45 @@
 import './Calculator.css';
-import React from 'react';
-import calculate from './logic/calculate';
+import React, { useState } from 'react';
 import Key from './Key';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.keyPress = this.keyPress.bind(this);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-  }
-
-  keyPress(name) {
-    const result = calculate(this.state, name);
-    this.setState(result);
-  }
-
-  render() {
-    const { total, operation, next } = this.state;
-    return (
-      <div className="container">
-        <div className="display">
-          <p>{total || ''}</p>
-          <p>{operation || ''}</p>
-          <p>{next || ''}</p>
-        </div>
-        <div className="keys">
-          <Key classes="key" name="AC" keyPress={this.keyPress} />
-          <Key classes="key" name="+/-" keyPress={this.keyPress} />
-          <Key classes="key" name="%" keyPress={this.keyPress} />
-          <Key classes="key symbol" name="÷" keyPress={this.keyPress} />
-          <Key classes="key" name={7} keyPress={this.keyPress} />
-          <Key classes="key" name={8} keyPress={this.keyPress} />
-          <Key classes="key" name={9} keyPress={this.keyPress} />
-          <Key classes="key symbol" name="&times;" keyPress={this.keyPress} />
-          <Key classes="key" name={4} keyPress={this.keyPress} />
-          <Key classes="key" name={5} keyPress={this.keyPress} />
-          <Key classes="key" name={6} keyPress={this.keyPress} />
-          <Key classes="key symbol" name="-" keyPress={this.keyPress} />
-          <Key classes="key" name={1} keyPress={this.keyPress} />
-          <Key classes="key" name={2} keyPress={this.keyPress} />
-          <Key classes="key" name={3} keyPress={this.keyPress} />
-          <Key classes="key symbol" name="+" keyPress={this.keyPress} />
-          <Key classes="key zero" name={0} keyPress={this.keyPress} />
-          <Key classes="key" name="." keyPress={this.keyPress} />
-          <Key classes="key symbol" name="=" keyPress={this.keyPress} />
-        </div>
-      </div>
-    );
-  }
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+  
+  const { total, operation, next } = state;
+  return (
+    <div className="container">
+    <div className="display">
+      <p>{total || ''}</p>
+      <p>{operation || ''}</p>
+      <p>{next || ''}</p>
+    </div>
+    <div className="keys">
+      <Key classes="key" name="AC" state={state} setState={setState} />
+      <Key classes="key" name="+/-" state={state} setState={setState} />
+      <Key classes="key" name="%" state={state} setState={setState} />
+      <Key classes="key symbol" name="÷" state={state} setState={setState} />
+      <Key classes="key" name="7" state={state} setState={setState} />
+      <Key classes="key" name="8" state={state} setState={setState} />
+      <Key classes="key" name="9" state={state} setState={setState} />
+      <Key classes="key symbol" name="&times;" state={state} setState={setState} />
+      <Key classes="key" name="4" state={state} setState={setState} />
+      <Key classes="key" name="5" state={state} setState={setState} />
+      <Key classes="key" name="6" state={state} setState={setState} />
+      <Key classes="key symbol" name="-" state={state} setState={setState} />
+      <Key classes="key" name="1" state={state} setState={setState} />
+      <Key classes="key" name="2" state={state} setState={setState} />
+      <Key classes="key" name="3" state={state} setState={setState} />
+      <Key classes="key symbol" name="+" state={state} setState={setState} />
+      <Key classes="key zero" name="0" state={state} setState={setState} />
+      <Key classes="key" name="." state={state} setState={setState} />
+      <Key classes="key symbol" name="=" state={state} setState={setState} />
+    </div>
+  </div> 
+  );
 }
 
 export default Calculator;
