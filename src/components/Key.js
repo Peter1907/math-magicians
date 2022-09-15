a49b3af580fe7f0b8982423538f3cf26b433f979
Peter1907 <peter.nady.1408@gmail.com>
2022-09-15 17:44:49 +0200

    Fix linter errors

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ReactPropTypes } from 'react';
 
 class Key extends React.Component {
   keyPress = (e) => {
@@ -10,8 +11,14 @@ class Key extends React.Component {
     const { classes, name } = this.props;
     return (
       <button type="button" className={classes} onClick={this.keyPress}>{name}</button>
-    )
+    );
   }
+}
+
+Key.propTypes = {
+  keyPress: ReactPropTypes.func.isRequired,
+  classes: ReactPropTypes.string.isRequired,
+  name: ReactPropTypes.string,
 }
 
 export default Key;
