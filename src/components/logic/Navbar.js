4936bec39469fc1461791fdfb18a9f923a09f497
Peter1907 <peter.nady.1408@gmail.com>
2022-09-21 17:31:13 +0200

    Fix linter errors

--- src/components/logic/Navbar.js
+++ src/components/logic/Navbar.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Navbar = () =>
-<section className="nav-container">
+const Navbar = () => (
+  <section className="nav-container">
   <h1 style={{ fontSize: '50px', margin: '5px 0', fontFamily: "'Bungee Spice', cursive" }}>Math Magicians</h1>
   <ul className="links">
     <li><Link to="/" className="link">Home</Link></li>
@@ -11,6 +11,7 @@ const Navbar = () =>
     <li className="separator" />
     <li><Link to="/quote" className="link">Quote</Link></li>
   </ul>
-</section>;
+</section>
+);
 
 export default Navbar;
